83c70e76be0c3b9975f905181abc389e29f769c1
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-09-16 10:16:45 +0200

    Add AutoMapper, MediatR and extension method for them.

---
+++ BugTracker.Application/AppServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace BugTracker.Application
+{
+    public static class AppServiceRegistration
+    {
+        public static IServiceCollection AddAppServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            return services;
+        }
+    }
+}
