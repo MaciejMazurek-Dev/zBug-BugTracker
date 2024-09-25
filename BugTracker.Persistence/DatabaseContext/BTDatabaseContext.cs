9c64584db0e21be1aa3bb9a3dcfabf56d7a02897
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-09-25 12:51:00 +0200

    Add database context to Infrastructure

---
+++ BugTracker.Persistence/DatabaseContext/BTDatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using BugTracker.Domain;
+using BugTracker.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Persistence.DatabaseContext
+{
+    public class BTDatabaseContext : DbContext
+    {
+        public BTDatabaseContext(DbContextOptions<BTDatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<IssuePriority> IssuePriorities { get; set; }
+        public DbSet<IssueStatus> IssueStatuses { get; set; }
+        public DbSet<IssueType> IssueTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BTDatabaseContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in base.ChangeTracker.Entries<BaseEntity>()
+                .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+            {
+                entry.Entity.DateModified = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
