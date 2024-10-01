0d4bec6d6eea55eeb30cb84f9036674b96c7f329
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-01 11:42:59 +0200

    Fix wrong DTO property types in Core.Application

--- BugTracker.Application/Features/Issue/Queries/GetAllIssues/IssueDto.cs
+++ BugTracker.Application/Features/Issue/Queries/GetAllIssues/IssueDto.cs
@@ -1,4 +1,7 @@
-﻿using BugTracker.Domain;
+﻿using BugTracker.Application.Features.IssuePriority.Queries.GetAllIssuePriorities;
+using BugTracker.Application.Features.IssueStatus.Queries.GetAllIssueStatuses;
+using BugTracker.Application.Features.IssueType.Queries.GetAllIssueTypes;
+using BugTracker.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,9 @@ namespace BugTracker.Application.Features.Issue.Queries.GetAllIssues
     {
         public int Id { get; set; }
         public int ProjectId { get; set; }
-        public Domain.IssueType? IssueType { get; set; }
-        public Domain.IssueStatus? IssueStatus { get; set; }
-        public Domain.IssuePriority? IssuePriority { get; set; }
+        public IssueTypeDto IssueType { get; set; }
+        public IssueStatusDto IssueStatus { get; set; }
+        public IssuePriorityDto IssuePriority { get; set; }
         public string Summary { get; set; } = string.Empty;
         public int ReporterId { get; set; }
         public int Assignee { get; set; }
--- BugTracker.Application/Features/Issue/Queries/GetIssueById/IssueByIdDto.cs
+++ BugTracker.Application/Features/Issue/Queries/GetIssueById/IssueByIdDto.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BugTracker.Application.Features.IssuePriority.Queries.GetAllIssuePriorities;
+using BugTracker.Application.Features.IssueStatus.Queries.GetAllIssueStatuses;
+using BugTracker.Application.Features.IssueType.Queries.GetAllIssueTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,13 @@ namespace BugTracker.Application.Features.Issue.Queries.GetIssueById
 {
     public class IssueByIdDto
     {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public IssueTypeDto IssueType { get; set; }
+        public IssueStatusDto IssueStatus { get; set; }
+        public IssuePriorityDto IssuePriority { get; set; }
+        public string Summary { get; set; } = string.Empty;
+        public int ReporterId { get; set; }
+        public int Assignee { get; set; }
     }
 }
