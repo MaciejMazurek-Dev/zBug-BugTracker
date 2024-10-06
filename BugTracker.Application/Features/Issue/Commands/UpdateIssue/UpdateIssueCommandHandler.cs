ec377ca065b2b87d981360c6c7f6207a3f722dd0
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-06 15:39:30 +0200

    Add UpdateIssue feature to Core.Application

---
+++ BugTracker.Application/Features/Issue/Commands/UpdateIssue/UpdateIssueCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BugTracker.Application.Contracts.Persistence;
+using MediatR;
+
+namespace BugTracker.Application.Features.Issue.Commands.UpdateIssue
+{
+    public class UpdateIssueCommandHandler : IRequestHandler<UpdateIssueCommand, Unit>
+    {
+        private readonly IIssueRepository _issueRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateIssueCommandHandler(IIssueRepository issueRepository, IMapper mapper)
+        {
+            _issueRepository = issueRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateIssueCommand request, CancellationToken cancellationToken)
+        {
+            var issue = _mapper.Map<Domain.Issue>(request);
+            await _issueRepository.UpdateAsync(issue);
+            return Unit.Value;
+        }
+    }
+}
