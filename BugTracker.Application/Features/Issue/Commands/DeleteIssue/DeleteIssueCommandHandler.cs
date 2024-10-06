370ab18e44725aca96273e6397a83915c11ee90b
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-06 15:10:57 +0200

    Add DeleteIssue feature to Core.Application

---
+++ BugTracker.Application/Features/Issue/Commands/DeleteIssue/DeleteIssueCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using BugTracker.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Features.Issue.Commands.DeleteIssue
+{
+    public class DeleteIssueCommandHandler : IRequestHandler<DeleteIssueCommand, Unit>
+    {
+        private readonly IIssueRepository _issueRepository;
+
+        public DeleteIssueCommandHandler(IIssueRepository issueRepository)
+        {
+            _issueRepository = issueRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteIssueCommand request, CancellationToken cancellationToken)
+        {
+            var issue = await _issueRepository.GetByIdAsync(request.Id);
+
+            await _issueRepository.DeleteAsync(issue);
+            return Unit.Value;
+        }
+    }
+}
