599210898948aace51b1ae3bce810b8c45d8fee9
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-06 11:13:28 +0200

    Add ConvertApiExceptions method to BaseHttpService in BlazorUI

--- BugTracker.BlazorUI/Services/Base/BaseHttpService.cs
+++ BugTracker.BlazorUI/Services/Base/BaseHttpService.cs
@@ -8,5 +8,36 @@
         {
             _client = client;
         }
+
+        public Response<Guid> ConvertApiExceptions(ApiException ex)
+        {
+            if (ex.StatusCode == 400)
+            {
+                return new Response<Guid>()
+                {
+                    Message = "Invalid data",
+                    ValidationErrors = ex.Response,
+                    Success = false
+                };
+            }
+            else if (ex.StatusCode == 404)
+            {
+                return new Response<Guid>()
+                {
+                    Message = "Record not found",
+                    ValidationErrors = ex.Response,
+                    Success = false
+                };
+            }
+            else
+            {
+                return new Response<Guid>()
+                {
+                    Message = "Something went wrong, please try later",
+                    ValidationErrors = ex.Response,
+                    Success = false
+                };
+            }
+        }
     }
 }
