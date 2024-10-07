7867d1ee35deaded6b38f65bb79da6d8f3c20200
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-07 13:54:54 +0200

    Add CreateIssueValidator to Core.Application

--- BugTracker.Application/Features/Issue/Commands/CreateIssue/CreateIssueCommand.cs
+++ BugTracker.Application/Features/Issue/Commands/CreateIssue/CreateIssueCommand.cs
@@ -5,9 +5,9 @@ namespace BugTracker.Application.Features.Issue.Commands.CreateIssue
     public class CreateIssueCommand : IRequest<Unit>
     {
         public int ProjectId { get; set; }
-        public int IssueTypeId { get; set; }
-        public int IssueStatusId { get; set; }
-        public int IssuePriopertyId { get; set; }
+        public int TypeId { get; set; }
+        public int StatusId { get; set; }
+        public int PriorityId { get; set; }
         public string Summary { get; set; } = string.Empty;
         public int ReporterId { get; set; }
         public int Assignee { get; set; }
---
+++ BugTracker.Application/Features/Issue/Commands/CreateIssue/CreateIssueValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Features.Issue.Commands.CreateIssue
+{
+    public class CreateIssueValidator : AbstractValidator<CreateIssueCommand>
+    {
+        public CreateIssueValidator()
+        {
+            RuleFor(issue => issue.Assignee)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(issue => issue.PriorityId)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(issue => issue.ProjectId)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(issue => issue.ReporterId)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(issue => issue.StatusId)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(issue => issue.Summary)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(issue => issue.TypeId)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
