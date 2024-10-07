c942193615e45484076c1b2ba09841b701fb3ddf
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-07 13:35:17 +0200

    Add NotFoundException to Core.Application

---
+++ BugTracker.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key) : base($"{name} ({key}) was not found.")
+        {
+        }
+    }
+}
+  
