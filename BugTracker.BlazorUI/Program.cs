34014af36292a3a6bdac8ad28034197f967d7018
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-10 09:28:52 +0200

    Add Auto Mapper to BlazorUI

--- BugTracker.BlazorUI/Program.cs
+++ BugTracker.BlazorUI/Program.cs
@@ -1,6 +1,7 @@
 using BugTracker.BlazorUI.Services.Base;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Reflection;
 
 namespace BugTracker.BlazorUI
 {
@@ -11,6 +12,8 @@ namespace BugTracker.BlazorUI
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
             builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7082"));
 
