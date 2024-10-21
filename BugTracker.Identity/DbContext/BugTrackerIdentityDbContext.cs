c1c7b1e774405b506c5ff20faab6223638a23de3
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-21 16:42:23 +0200

    Add IdentityDbContext to Infrastructure.Identity

---
+++ BugTracker.Identity/DbContext/BugTrackerIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BugTracker.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Identity.DbContext
+{
+    public class BugTrackerIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BugTrackerIdentityDbContext(DbContextOptions<BugTrackerIdentityDbContext> options) 
+            : base(options)
+        { }
+    }
+}
