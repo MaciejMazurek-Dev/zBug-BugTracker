63aafdc73adc7ab2be5cecd61168f7f4a890fe1b
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-21 15:06:31 +0200

    Add AuthService to Infrastructure.Identity

--- BugTracker.Application/Models/Identity/RegisterResponse.cs
+++ BugTracker.Application/Models/Identity/RegisterResponse.cs
@@ -2,6 +2,6 @@
 {
     public class RegisterResponse
     {
-        public int UserId { get; set; }
+        public string UserId { get; set; }
     }
 }
---
+++ BugTracker.Identity/Services/AuthService.cs
@@ -0,0 +1,102 @@
+﻿using BugTracker.Application.Contracts.Identity;
+using BugTracker.Application.Exceptions;
+using BugTracker.Application.Models.Identity;
+using BugTracker.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BugTracker.Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthService(
+            SignInManager<ApplicationUser> signInManager, 
+            UserManager<ApplicationUser> userManager, 
+            IOptions<JwtSettings> jwtSettings )
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public async Task<LoginResponse> Login(LoginRequest loginRequest)
+        {
+            var user = await _userManager.FindByEmailAsync(loginRequest.Email);
+
+            if (user == null)
+            {
+                throw new NotFoundException($"User {loginRequest.Email} not found.", loginRequest.Email);
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginRequest.Password, false);
+
+            if (result.Succeeded == false)
+            {
+                throw new BadRequestException($"Wrong password for user: {loginRequest}.");
+            }
+
+            JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+
+            var loginResponse = new LoginResponse
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken)
+            };
+
+            return loginResponse;
+        }
+
+        public async Task<RegisterResponse> Register(RegisterRequest registrationRequest)
+        {
+            var user = new ApplicationUser
+            {
+                Email = registrationRequest.Email,
+                FirstName = registrationRequest.FirstName,
+                LastName = registrationRequest.LastName
+            };
+
+            var result = await _userManager.CreateAsync(user, registrationRequest.Password);
+
+            if(result.Succeeded)
+            {
+                return new RegisterResponse(){ UserId = user.Id };
+            }
+            else
+            {
+                throw new BadRequestException($"{result.Errors}");
+            }
+        }
+
+        private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+
+            var tokenClaims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            }
+            .Union(userClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: tokenClaims,
+                expires: DateTime.Now.AddMinutes(_jwtSettings.DurationInMinutes),
+                signingCredentials: signingCredentials);
+
+            return jwtSecurityToken;
+        }
+    }
+}
