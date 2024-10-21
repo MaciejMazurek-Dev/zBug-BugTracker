932cec2b354943c32c8d2164b6b350530f8640cb
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-21 16:43:15 +0200

    Add IdentityServiceRegistration to Infrastructure. Identity

---
+++ BugTracker.Identity/IdentityServiceRegistration.cs
@@ -0,0 +1,54 @@
+﻿using BugTracker.Application.Models.Identity;
+using BugTracker.Identity.DbContext;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using BugTracker.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using BugTracker.Application.Contracts.Identity;
+using BugTracker.Identity.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BugTracker.Identity
+{
+    public static class IdentityServiceRegistration
+    {
+        public static IServiceCollection AddIdentityService(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+            services.AddDbContext<BugTrackerIdentityDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("BTIdentityConnection")));
+
+            services.AddIdentityCore<ApplicationUser>()
+                .AddEntityFrameworkStores<BugTrackerIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddTransient<IAuthService, AuthService>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateAudience = true,
+                    ValidIssuer = configuration["JwtSettings:Issuer"],
+                    ValidAudience = configuration["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]))
+                };
+            });
+
+            
+        }
+    }
+}
