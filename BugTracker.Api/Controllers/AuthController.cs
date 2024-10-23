ba23ab7817b0477547759ebbb6a64d91dc71dd44
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-23 11:28:46 +0200

    Add AuthController to Api

---
+++ BugTracker.Api/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using BugTracker.Application.Contracts.Identity;
+using BugTracker.Application.Models.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authenticationService;
+
+        public AuthController(IAuthService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponse>> Login(LoginRequest loginRequest)
+        {
+            LoginResponse response = await _authenticationService.Login(loginRequest);
+            return Ok(response);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegisterResponse>> Register(RegisterRequest registerRequest)
+        {
+            RegisterResponse response = await _authenticationService.Register(registerRequest);
+            return Ok(response);
+        }
+    }
+}
