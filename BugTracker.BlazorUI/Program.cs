9284ad963853bca670123461e2d3e827708db724
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-10-24 10:56:30 +0200

    Add Blazored.LocalStorage to BlazorUI

--- BugTracker.BlazorUI/Program.cs
+++ BugTracker.BlazorUI/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using BugTracker.BlazorUI.Services.Base;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,10 +13,12 @@ namespace BugTracker.BlazorUI
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
+            
             builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
             builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7082"));
+
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
