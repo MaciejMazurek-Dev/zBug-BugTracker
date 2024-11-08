80d5e752023f3c2a88ab81568e5bbc0927fa765f
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-11-08 20:32:42 +0100

    Refactor HttpClientService in BlazorUI

---
+++ BugTracker.BlazorUI/Services/HttpClient/HttpClientService.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http.Headers;
+
+namespace BugTracker.BlazorUI.Services.Base
+{
+    public class HttpClientService
+    {
+        protected readonly IClient _client;
+        protected readonly ILocalStorageService _localStorage;
+
+        public HttpClientService(IClient client, ILocalStorageService localStorage)
+        {
+            _client = client;
+            _localStorage = localStorage;
+        }
+
+        public async Task AddJwtToken()
+        {
+            string? token = await _localStorage.GetItemAsync<string>("token");
+
+            if (token != null)
+            {
+                _client.HttpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+    }
+}
