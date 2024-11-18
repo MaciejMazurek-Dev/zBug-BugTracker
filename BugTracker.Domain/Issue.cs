407ae1cf8885e788f7b7afa3e29551d5ac47524c
MaciejMazurek-Dev <maciejmazurek.praca@gmail.com>
2024-11-18 09:37:07 +0100

    Refactor Issue entity in Core.Domain

--- BugTracker.Domain/Issue.cs
+++ BugTracker.Domain/Issue.cs
@@ -4,12 +4,17 @@ namespace BugTracker.Domain
 {
     public class Issue : BaseEntity
     {
-        public int ProjectId { get; set; }
-        public IssueType? IssueType { get; set; }
-        public IssueStatus? IssueStatus { get; set; }
-        public IssuePriority? IssuePriority { get; set; }
+        public IssueType IssueType { get; set; }
+        public int IssueTypeId { get; set; }
+
+        public IssueStatus IssueStatus { get; set; }
+        public int IssueStatusId { get; set; }
+
+        public IssuePriority IssuePriority { get; set; }
+        public int IssuePriorityId { get; set; }
+
         public string Summary { get; set; } = string.Empty;
         public string ReporterId { get; set; } = string.Empty;
-        public string Assignee { get; set; } = string.Empty;
+        public string? AssigneeId { get; set; } = null;
     }
 }
